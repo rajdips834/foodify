a35d92c263d58869168c69e341f788f009d6074a
Rajdip Sinha <rajdips834@gmail.com>
2024-09-05 01:53:01 +0530

    Responsiveness added to caraousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -17,22 +17,22 @@ import { SingleFoodItem } from "../../components/FoodItem";
 import { Title } from "../../components/Sections";
 const Caraousel = () => {
   const [{ foodItems }, dispatch] = useStateValue();
+  const isSmallScreen = window.innerWidth < 640; // Check if the screen is smaller than 640px
 
   return (
-    <div className="pt-[40px]">
+    <div className="pt-[40px] w-full overflow-hidden">
       <div className="flex items-center justify-center w-full pb-[40px]">
         <Title title={"Trending Dishes"} center />
       </div>
-      <div className="p-4 overflow-hidden ">
+      <div className="">
         <Carousel
-          centerSlidePercentage={15}
+          centerSlidePercentage={isSmallScreen ? 70 : 30}
           centerMode={true}
           autoPlay // Enables automatic sliding
           infiniteLoop // Allows the slider to loop infinitely
           showThumbs={false} // Hides the thumbnail navigation
           showStatus={false} // Hides the status indicator
           interval={3000} // Sets the interval between slides (in milliseconds)
-          showArrows={true}
         >
           {foodItems &&
             foodItems.map((item: FoodItem) => (
